0e220f379117ada2cdfd0ad49528a30d17116dc4
John Diegor <john.diegor@gmail.com>
2018-07-22 14:49:01 +0800

    moved ammap javascript initialization in component

--- resources/views/maps/index.blade.php
+++ resources/views/maps/index.blade.php
@@ -1,53 +1,16 @@
 @extends('maps')
 
 @section('main-content')
-  @component('components.map', ['data' => $data])
+  @component('components.map', ['data' => $data, 'map' => 'usaLow'])
     @slot('title')
       Licensed Doctors Map
     @endslot
+
+    <div class="row mt-3">
+      <div class="col-12">
+        <p>Sunt irure ad deserunt laborum in enim proident sunt officia sunt non esse. Proident non eu laboris do enim mollit eu minim adipisicing nulla irure esse
+          et tempor tempor. Ut occaecat officia nisi cillum mollit dolore ex magna enim ex incididunt.</p>
+      </div>
+    </div>
   @endcomponent
 @endsection
-
-@push('scripts')
-<script type="text/javascript">
-var map = AmCharts.makeChart( "mapdiv", {
-  type: "map",
-  theme: "light",
-  dataProvider: {
-    map: "usaLow",
-    areas: @json($data)
-  },
-  areasSettings: {
-    color: "#3697d9",
-    rollOverColor: "#9acbec",
-    rollOverOutlineColor: "#256997",
-    selectedColor: "#3087c3",
-    outlineColor: "#FFFFFF",
-    balloonText: "<strong>[[title]]</strong><br/>Licensed doctor: [[description]]"
-  },
-  imagesSettings: {
-    labelPosition: "middle",
-    labelFontSize: 9
-  }
-});
-
-map.addListener("init", function() {
-  setTimeout(function() {
-    // iterate through areas and put a label over center of each
-    map.dataProvider.images = [];
-    for ( x in map.dataProvider.areas ) {
-      var area = map.dataProvider.areas[ x ];
-      var image = new AmCharts.MapImage();
-      image.latitude = map.getAreaCenterLatitude( area );
-      image.longitude = map.getAreaCenterLongitude( area );
-      image.label = area.id.substr( 3 );
-      image.title = area.title;
-      image.linkToObject = area;
-      map.dataProvider.images.push( image );
-    }
-    map.validateData();
-    console.log( map.dataProvider );
-  }, 100)
-});
-</script>
-@endpush
